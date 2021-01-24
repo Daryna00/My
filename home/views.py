0cfcea11d92d9e8e372ad8f6889906456afe3bfb
Daryna00 <d.denderuk@gmail.com>
2021-01-24 18:07:56 +0200

    'heroku'

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
-"""from .forms import ContactForm, CommentForm"""
+from .forms import ContactForm
 from merchan.models import Merchandise
 from product.models import Product
 from .models import Order
